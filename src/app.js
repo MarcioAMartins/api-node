05c8d538d23eadb2b0b1f455405779e16359f96b
Marcio Alves <MarcioApocallypse@gmail.com>
2022-03-27 13:01:12 -0300

    api-node-start

---
+++ src/app.js
@@ -0,0 +1,19 @@
+import { openDb } from './configDB.js';
+import express from 'express';
+
+const app = express();
+app.use(express.json());
+
+openDb();
+
+app.get( '/', function(req, res){
+    res.send("ola mundo");
+});
+
+app.post('/pessoa',function(req, res){
+    console.log(req.body);
+    res.json({
+        "statusCode": 200
+    })
+});
+app.listen(3000,()=>console.log("api-rodando."))
